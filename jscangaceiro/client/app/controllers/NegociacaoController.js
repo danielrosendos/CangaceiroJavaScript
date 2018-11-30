31fd8cce0f1519bc25e0fa5c5d1bf08f47a36bc7
Daniel Rosendo <daniel.rosendos@hotmail.com>
2018-11-29 22:06:58 -0300

    Termino do Capitulo 5

--- jscangaceiro/client/app/controllers/NegociacaoController.js
+++ jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -7,25 +7,36 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
 
     }
 
     adiciona(event) {
+        
         //cancelando a submissão do formulário
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();    
 
-        let negociacao = new Negociacao(
+    }
+
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
 
-        console.log(negociacao);
-
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-
-        console.log(diaMesAno);
-    
     }
 
 }
---
+++ jscangaceiro/client/app/domain/negociacao/Negociacoes.js
@@ -0,0 +1,21 @@
+class Negociacoes {
+
+    constructor() {
+
+        this._negociacoes = [];
+
+    }
+
+    adiciona(negociacao) {
+
+        this._negociacoes.push(negociacao);
+
+    }
+
+    paraArray() {
+
+        return [].concat(this._negociacoes);
+
+    }
+
+}
