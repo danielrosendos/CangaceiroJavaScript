f7fe7a138640056fe4de307c3b2de2618281e00e
Daniel Rosendo <daniel.rosendos@hotmail.com>
2018-12-02 21:54:29 -0300

    Termino com Defeito Cap 10 Analisar Onde ta O erro

--- jscangaceiro/client/app/controllers/NegociacaoController.js
+++ jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -7,33 +7,18 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        /*
-        this._negociacoes = new Negociacoes(model => {
-            this._negociacoesView.update(model);
-        });
-        */
 
-        const self = this;
+        this._negociacoes = new Bind(
+            new Negociacoes(),
+            new NegociacoesView('#negociacoes'),
+            'adiciona', 'esvazia'
+        );
 
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            get(target, prop, receiver) {
-                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia'].includes(prop)) {
-                    return function() {
-                        console.log(`"${prop}" disparou a armadilha`);
-                        target[prop].apply(target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                } else {
-                    return target[prop];
-                }
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView('#negociacoes');
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView('#MensagemView');
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView('#mensagemView'),
+            'texto'
+        );
 
     }
 
@@ -43,7 +28,6 @@ class NegociacaoController {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();    
 
     }
@@ -71,7 +55,6 @@ class NegociacaoController {
 
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações Apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
 
     }
 
