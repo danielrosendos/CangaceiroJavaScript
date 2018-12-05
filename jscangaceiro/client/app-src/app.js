87334747fdf6166c516cdc235cfc3de392f17330
Daniel Rosendo <daniel.rosendos@hotmail.com>
2018-12-05 10:54:37 -0300

    +Modificaçoes CAP 20

--- jscangaceiro/client/app-src/app.js
+++ jscangaceiro/client/app-src/app.js
@@ -1,4 +1,7 @@
-import { NegociacaoController } from './controllers/NegociacaoController.js'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import '../css/meucss.css'
+import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
 
 const controller = new NegociacaoController();
